cc542c4c2083070fa2fd02ee07c3c4a82b11eeba
Boon Khang Hua <khang-hua.boon@hpe.com>
2023-06-15 17:37:06 +0800

    update

--- pds_version/test1/edges.py
+++ pds_version/test1/edges.py
@@ -1,24 +1,26 @@
 import cv2
 import csv
 import numpy as np
-#import pandas as pd
 from matplotlib import pyplot as plt
 import os
 import pickle
 
-# make_edges reads an image from /pfs/images and outputs the result of running
-# edge detection on that image to /pfs/out. Note that /pfs/images and
-# /pfs/out are special directories that Pachyderm injects into the container.
-def make_edges(image):
+# make_edges reads an image from /pfs/images and outputs the result of running edge detection on that image to /pfs/out.
+# Note that /pfs/images and /pfs/out are special directories that Pachyderm injects into the container.
+input_data_path = "/pfs/pipeline_input_data"
+pach_std_output_path = "pfs/out"
+output_data_path = os.path.join(pach_std_output_path, "mri_edges")
+
+def make_edges(image, output_data_path):
    img = cv2.imread(image)
    tail = os.path.split(image)[1]
    edges = cv2.Canny(img,100,200)
-   plt.imsave(os.path.join("/pfs/out", os.path.splitext(tail)[0]+'.png'), edges, cmap = 'gray')
+   plt.imsave(os.path.join(output_data_path, os.path.splitext(tail)[0]+'.png'), edges, cmap = 'gray')
 
 # walk /pfs/pipeline_input_data and call make_edges on every file found
 output_list = []
 counter=0
-for dirpath, dirs, files in os.walk("/pfs/pipeline_input_data"):
+for dirpath, dirs, files in os.walk(input_data_path):
    #print(dirpath)
    for file in files:
       filepath = os.path.join(dirpath, file)
@@ -28,7 +30,7 @@ for dirpath, dirs, files in os.walk("/pfs/pipeline_input_data"):
       #    break
       if filepath.split(".")[-1] == "tif":
          print('process file ', filepath)
-         make_edges(filepath)
+         make_edges(filepath, output_data_path)
       counter=counter+1
 
 # with open("/pfs/pipeline_input_data/kaggle_3m_dataset/data.csv", 'r') as file:
@@ -38,5 +40,5 @@ for dirpath, dirs, files in os.walk("/pfs/pipeline_input_data"):
     
 # df = pd.read_csv("/pfs/pipeline_input_data/kaggle_3m_dataset/data.csv")
 # print(df.head())
-with open(os.path.join("/pfs/out","data.pickle"), 'wb') as f:
+with open(os.path.join(output_data_path,"data.pickle"), 'wb') as f:
    pickle.dump(output_list, f)
