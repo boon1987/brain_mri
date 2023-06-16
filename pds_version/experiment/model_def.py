d70726b0d00b535ee3e55906daa9b5594ac48c78
Boon Khang Hua <khang-hua.boon@hpe.com>
2023-06-17 01:25:15 +0800

    update

--- pds_version/experiment/model_def.py
+++ pds_version/experiment/model_def.py
@@ -21,23 +21,20 @@ class MRIUnetTrial(PyTorchTrial):
         full_dir = "/"
 
         if training:
-            try:
-                download_dir = self.data_config["download_directory"]
-                data_dir = self.data_config["data_dir"]
-                full_dir = os.path.join(full_dir, download_dir.strip("/"), data_dir.strip("/"))
+            download_dir = self.data_config["download_directory"]
+            data_dir = self.data_config["data_dir"]
+            full_dir = os.path.join(full_dir, download_dir.strip("/"), data_dir.strip("/"))
 
-                des = self.download_data(self.data_config, full_dir)
+            des = self.download_data(self.data_config, full_dir)
 
-                print("Download Directory = " + full_dir)
+            print("Download Directory = " + full_dir)
 
-                self.train_dataset, self.val_dataset = data.get_train_val_datasets(
-                    download_dir,
-                    data_dir,
-                    self.context.get_hparam("split_seed"),
-                    self.context.get_hparam("validation_ratio"),
-                )
-            except:
-                pass
+            self.train_dataset, self.val_dataset = data.get_train_val_datasets(
+                download_dir,
+                data_dir,
+                self.context.get_hparam("split_seed"),
+                self.context.get_hparam("validation_ratio"))
+
 
         if training:
             try:
