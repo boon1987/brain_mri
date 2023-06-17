559fa883d54bb0141805e6e6e973e04dcef45b5d
Boon Khang Hua <khang-hua.boon@hpe.com>
2023-06-17 22:06:58 +0800

    update

--- pds_version/experiment/startup-hook.sh
+++ pds_version/experiment/startup-hook.sh
@@ -1,5 +1,5 @@
 pip install python_pachyderm==7.5.0
 #pip install python_pachyderm==7.3.0
-pip install scikit-image==0.19.2 
-pip install scikit-learn
-pip install opencv-python-headless
+#pip install scikit-image==0.19.2 
+#pip install scikit-learn
+#pip install opencv-python-headless
